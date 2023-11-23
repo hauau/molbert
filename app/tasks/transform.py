7a85123916430ac1473875a8dff4c4c54c352cf3
Sergei Shikalov <ssh@42px.ru>
2023-11-23 20:37:52 +0200

    feat: transform error mark

--- app/tasks/transform.py
+++ app/tasks/transform.py
@@ -1,5 +1,6 @@
 from asyncio import sleep
 from datetime import datetime
+import json
 import requests
 import base64
 from .upload import upload_stream_to_s3, get_image_buffer_test
@@ -11,28 +12,6 @@ from ..config import internal_ml_url, internal_ml_workspace
 import time
 from tempfile import SpooledTemporaryFile
 import httpx
-
-
-def apply_transform(image: Image):
-    if image.status == 'ready':
-        raise BaseException("Trying to transform image in status 'ready'")
-
-    match image.type:
-        case OperationType.bgRemoval:
-            print('bgRemoval ', image.image_id)
-            remove_background_native()
-        case OperationType.doubleResolution:
-            print('doubleResolution ', image.image_id)
-
-
-async def stream_response(url, req_body, headers):
-    with requests.post(url=url, json=req_body, headers=headers, stream=True, timeout=60) as response:
-        if response.status_code > 300:
-            print(response.content)
-        response.raise_for_status()
-        for chunk in response.iter_content(chunk_size=32000):
-            yield chunk  # This will yield raw byte-like chunks of the HTTP response
-
 
 async def internal_ml_call(image_base64: str, task: str, temp_file: SpooledTemporaryFile[bytes]):
     # TODO:    retry
@@ -51,8 +30,14 @@ async def internal_ml_call(image_base64: str, task: str, temp_file: SpooledTempo
 
     # For decoding chunks not suitable for decoding yet
     buffer = b""
+
+    # TODO: rm
     async with httpx.AsyncClient() as client:
         async with client.stream("POST", url=internal_ml_url, json=req_body, headers=headers, timeout=60) as response:
+            if response.is_error:
+                temp_file.close()
+                await response.aread()
+                return response.content
             async for chunk in response.aiter_bytes():
                 chunk = buffer + chunk
                 # Extract  b'{"predictions":"saddsfdfd...
@@ -70,7 +55,6 @@ async def internal_ml_call(image_base64: str, task: str, temp_file: SpooledTempo
                 if chunk_to_decode:
                     temp_file.write(base64.b64decode(
                         chunk_to_decode + b'==', validate=False))
-
 
 async def create_transformed_image(from_uuid: str, from_extension: str, to_uuid: str, task: OperationType, model: ModelType):
     # TODO: Chain with upload end, can't isolate completely from original
@@ -91,14 +75,27 @@ async def create_transformed_image(from_uuid: str, from_extension: str, to_uuid:
 
     # Step 5: Upload back to S3
     temp_file = SpooledTemporaryFile(max_size=1024)
-    
+    err = ""
     match model:
         case ModelType.internal:
             internal_task = 'background_remove' if task == OperationType.bgRemoval else 'super_resolution'
-            await internal_ml_call(base64_encoded, internal_task, temp_file)
+            err = await internal_ml_call(base64_encoded, internal_task, temp_file)
         case ModelType.ai24:
             raise BaseException("Not implemented")  
     
+    if temp_file.closed:
+        print("Image processing error: \n", json.dumps(err.decode('utf-8')))
+        with SessionLocal() as db:
+            db.execute(
+                update(Image)
+              .where(Image.image_id == to_uuid)
+              .values({"status": "error", "uploaded_at": datetime.now(), "transformed_at": datetime.now()})
+            )
+            db.commit()
+            db.close()
+            print("Transformed image marked as error")
+        return
+
     temp_file.seek(0)
 
     await upload_stream_to_s3(temp_file, to_uuid, from_extension)
