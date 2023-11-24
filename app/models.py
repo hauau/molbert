23e02782000ef5c93cd1c5f2be3952f942bd3bdb
Sergei Shikalov <ssh@42px.ru>
2023-11-24 16:13:41 +0200

    fix: add model_type to Image model

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class Image(Base):
     __tablename__ = "image"
     image_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     type = Column(Enum('original', 'background_remove', 'super_resolution', name='image_type'))
+    model_type = Column(Enum('internal', '24ai', name='model_type'))
     created_at = Column(DateTime(timezone=True))
     uploaded_at = Column(DateTime(timezone=True))
     transformed_at = Column(DateTime(timezone=True))
