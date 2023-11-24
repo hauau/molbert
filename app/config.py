ca3911f4c26f443b9fe0e38e1ee743009f2b712c
Sergei Shikalov <ssh@42px.ru>
2023-11-24 19:49:31 +0200

    feat: new config

--- app/config.py
+++ app/config.py
@@ -9,6 +9,9 @@ DEBUG_SQL = True if getenv("DEBUG_SQL", "false").lower() == 'true' else False
 name_service = getenv("ML_NAME_SERVICE","1700559467") 
 internal_ml_workspace = getenv("ML_WORKSPACE_ID","111111-11111-11111-11111-1111111") 
 internal_ml_url = f'https://mlspace.aicloud.sbercloud.ru/deployments/dgx2-inf-001/kfserving-{name_service}/v1/models/kfserving-{name_service}:predict'
+# 24ai token
+ml_24ai_token = getenv("ML_24AI_TOKEN","0b52cfe4-e13b-418a-9096-c4840da1f226") 
+ml_24ai_url = getenv("ML_24AI_URL","https://core.24ai.tech/api/v1/") 
 
 # S3 Configuration
 AWS_ACCESS_KEY_ID = getenv("AWS_ACCESS_KEY_ID", "minioadmin")
